4cb287d0ba852b4b6660e2475a801994ad7b9111
joeflorepoma <jrm62@hotmail.com>
2018-09-10 21:22:05 -0300

    para tomar una cadena de caracters sin scanf

---
+++ fgets/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char buffer[64];
+    int cantidad;
+
+    fgets(buffer,sizeof(buffer)-2,stdin);
+
+    cantidad = strlen(buffer);
+
+    buffer[cantidad-1] = '\0';
+
+    printf("%s", buffer);
+
+    return 0;
+}
